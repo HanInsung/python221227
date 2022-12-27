163f56859085922fb5f53538feb1877825f9610d
HanInsung <p9741181@gmail.com>
2022-12-27 15:02:49 +0900

    연습3

---
+++ Second-Day/5_class1.py
@@ -0,0 +1,14 @@
+#class1.py
+#1) Class Define
+class Person:
+    def __init__(self): #create method 
+        self.name = "default name"
+
+    def print(self):
+        print("My Name is {0}".format(self.name))
+
+#2) Creation Instance copy 
+p1 = Person()
+
+#3) Call Method
+p1.print()
