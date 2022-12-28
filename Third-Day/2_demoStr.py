9354bf8757deaa880fb53f4f51b3c8b5f499ab3c
HanInsung <p9741181@gmail.com>
2022-12-28 16:20:50 +0900

    정규표현식 연습2

--- Third-Day/2_demoStr.py
+++ Third-Day/2_demoStr.py
@@ -35,10 +35,34 @@ print( ":)".join(lst))
 
 import re
 
-result = re.search("[0-9]*th","35th")
+result = re.search("[0-9]*th","  35th")
 print( result)
 print(result.group())
 
-result = re.match("[0-9]*th","35th")
-print(result)
+# result = re.match("[0-9]*th","  35th")
+# print(result)
+# print(result.group())
+
+#result = re.search("apple","this is apple")
+result = re.search("apple","this is Apple".lower())
 print(result.group())
+result = re.search("\d{4}","올해는 2022년입니다.")
+print(result.group())
+
+
+result = re.search("\d{5}","우리 동제는 52134")
+
+print(result.group())
+
+print("다중라인 ---")
+strM = """ 이 문자열은 
+다음과 같이 
+
+빈줄도 있습니다. """
+
+#^:시작할때, .:글자 하나, +:하나라도(출현회수)
+
+carot = re.compile("^.+", re.M)
+print(carot.findall(strM))
+
+
