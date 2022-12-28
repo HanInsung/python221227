d45b5f99edc6c6b01cc293a76c3d5540ea072c30
HanInsung <p9741181@gmail.com>
2022-12-28 16:09:22 +0900

    re 매칭

--- Third-Day/2_demoStr.py
+++ Third-Day/2_demoStr.py
@@ -32,3 +32,13 @@ print(lst)
 print("--다시합치기--")
 print( ":)".join(lst))
 
+
+import re
+
+result = re.search("[0-9]*th","35th")
+print( result)
+print(result.group())
+
+result = re.match("[0-9]*th","35th")
+print(result)
+print(result.group())
