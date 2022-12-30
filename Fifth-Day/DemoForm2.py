4601801ef0cd1b1d3370d040aca9c3b2228a6a8a
HanInsung <p9741181@gmail.com>
2022-12-30 11:09:13 +0900

    gui 연습2

--- Fifth-Day/DemoForm2.py
+++ Fifth-Day/DemoForm2.py
@@ -5,8 +5,11 @@ import sys
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
 
+import urllib.request
+from bs4 import BeautifulSoup
+
 #변경
-form_class = uic.loadUiType("c:\\work\\Fifth-Day\\DemoForm2.ui")[0]
+form_class = uic.loadUiType("DemoForm2.ui")[0]
 
 #폼 클래스(QMainWindow)
 class DemoForm(QMainWindow, form_class):
@@ -17,7 +20,35 @@ class DemoForm(QMainWindow, form_class):
         self.label.setText("첫번째 Qt 데모")
 
     def firstClick(self):
-        self.label.setText("첫번째 버튼")
+        f_handle = open("webtoon.txt","wt", encoding="utf-8")
+
+        for i in range(1,6):
+            url = "http://comic.naver.com/webtoon/list.nhn?titleId=20853&weekday=fri&page=" + str(i)
+            #print(url)
+
+            data = urllib.request.urlopen(url)
+
+            soup = BeautifulSoup(data,"html.parser")
+
+            cartoons = soup.find_all("td",class_="title")
+            #print("개수:{0}".format(len(cartoons)))
+            #carot = re.compile("^.+", re.M)
+            
+            for item in cartoons:
+                title = item.find("a").text
+                link = item.find("a")["href"]
+                title_item = title.split("<")
+                title_item = title_item[1][:-1]
+                #m = re.search('<(.*?)>', title_item)
+                #print (m.group(1))
+
+                print (title_item)
+                f_handle.write(title + "\n")
+
+                #print(link)
+
+        f_handle.close()
+            
     
     def secondClick(self):
         self.label.setText("두번째 버튼")
